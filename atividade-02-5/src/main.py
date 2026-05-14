5412fbf363a3b2ef19daaec942b90a33f47fb567
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2026-05-14 13:05:43 -0300

    feat(activity 2.5): add visit counter using cookies

--- atividade-02-5/src/main.py
+++ atividade-02-5/src/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from atividade-02-5!")
+from flask import Flask, make_response, render_template, request
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def counter():
+    if "counter" in request.cookies:
+        count = int(request.cookies.get("counter"))
+    else:
+        count = 1
+
+    resp = make_response(render_template("counter.html", count=count))
+    resp.set_cookie("counter", str(count + 1), max_age=10 * 365 * 24 * 60 * 60)
+
+    return resp
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
